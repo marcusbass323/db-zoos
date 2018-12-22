25e7a3bcba95906fbd9b765748be0f0c0ba827d6
marcusbass323 <marcus@bassconsultantgroup.com>
2018-12-21 23:34:35 -0800

    Added knex and sqlite3

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
-
-server.post((req, res) => {
-  
-})
-
 const port = 3300;
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  console.log('zoo info', zoo)
+  db('zoo').insert(zoo)
+      .then(ids => {
+          res.status(201).json(ids);
+      })
+      .catch(err => {
+          res.status(500).json({
+              err: 'Failed to insert zoo'
+          });
+      });
+});
+
+
+
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
